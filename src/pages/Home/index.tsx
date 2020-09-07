b366d05c87d85e5667ac561ac437ceb997351453
Vanessa Maria <vanessamfc@outlook.com>
2020-09-07 17:12:39 -0300

    css fixes

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import dogImg from "../../assets/dogImg.png";
 import { Container } from "./styles";
+
+import { FiSearch } from "react-icons/fi";
 
 const Home: React.FC = () => {
   return (
     <Container>
-      <h1>Findadogo</h1>
-      <p>Encontre aqui o seu melhor amigo</p>
-      <Link to="/add-dog">aa</Link>
+      <div>
+        <h1>Findadogo</h1>
+        <p>Encontre aqui o seu melhor amigo</p>
+        <Link to="/add-dog">
+          <span>
+            <FiSearch />
+          </span>
+          <strong>clique aqui por enquanto</strong>
+        </Link>
+      </div>
+      <div>
+        <img src={dogImg} alt="" />
+      </div>
     </Container>
   );
 };
