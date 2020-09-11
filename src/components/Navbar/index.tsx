0ecb3ac9a3b43519a6ca11c68e2bb5abda7929d6
Vanessa Maria <vanessamfc@outlook.com>
2020-09-10 21:06:59 -0300

    navbar added

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import { FaPaw } from 'react-icons/fa';
+import logo from '../../assets/paw.png';
+import { Container, StyledNavLink } from './styles';
+
+const Navbar: React.FC = () => {
+  return (
+    <Container className="navBar">
+      <div>
+        <Link to="/">
+          <FaPaw />
+        </Link>
+      </div>
+      <div>
+        <StyledNavLink to="/add-dog">Adicionar</StyledNavLink>
+        <StyledNavLink to="/list">Lista</StyledNavLink>
+      </div>
+    </Container>
+  );
+};
+
+export default Navbar;
